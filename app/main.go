63da9ff338f0152c4f702c96705dd726c33b1ed4
ikuo89 <discord.890407@gmail.com>
2019-05-15 20:16:58 +0900

    db接続

--- app/main.go
+++ app/main.go
@@ -1,27 +1,98 @@
 package main
 
 import (
+  // "fmt"
+  // "log"
+  // "os"
+  "time"
+  "net/http"
   "github.com/gin-gonic/gin"
   "gopkg.in/olahol/melody.v1"
+  "github.com/jinzhu/gorm"
+  _ "github.com/lib/pq"
 )
 
+type User struct {
+  gorm.Model
+  Name string `json:"name"`
+}
+
+type Users []User
+
 func main() {
-	r := gin.Default()
+  r := gin.Default()
   m := melody.New()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "index",
-		})
-	})
+  db, err := gorm.Open("postgres", "host=db user=root password=root dbname=ec_concier_chat sslmode=disable")
+  if err != nil {
+    panic(err.Error())
+  }
+  defer db.Close()
+  db.AutoMigrate(User{})
+  db.LogMode(true)
+
+  r.GET("/", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "index",
+    })
+  })
 
   r.GET("/ws", func(c *gin.Context) {
-		m.HandleRequest(c.Writer, c.Request)
-	})
+    m.HandleRequest(c.Writer, c.Request)
+  })
 
-	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		m.Broadcast(msg)
-	})
+  m.HandleMessage(func(s *melody.Session, msg []byte) {
+    m.Broadcast(msg)
+  })
 
-	r.Run() // listen and serve on 0.0.0.0:8080
+  r.POST("/users", func(c *gin.Context) {
+    data := User{}
+    now := time.Now()
+    data.CreatedAt = now
+    data.UpdatedAt = now
+
+    if err := c.BindJSON(&data); err != nil {
+      c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
+    }
+    db.NewRecord(data)
+    db.Create(&data)
+    if db.NewRecord(data) == false {
+      c.JSON(http.StatusOK, data)
+    }
+  })
+
+  r.GET("/users", func(c *gin.Context) {
+    users := []User{}
+    db.Find(&users)
+    c.JSON(http.StatusOK, users)
+  })
+
+  r.GET("/users/:id", func(c *gin.Context) {
+    user := User{}
+    id := c.Param("id")
+
+    db.Where("ID = ?", id).First(&user)
+    c.JSON(http.StatusOK, user)
+  })
+
+  r.PUT("/users/:id", func(c *gin.Context) {
+    user := User{}
+    id := c.Param("id")
+
+    data := User{}
+    if err := c.BindJSON(&data); err != nil {
+      c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
+    }
+
+    db.Where("ID = ?", id).First(&user).Updates(&data)
+  })
+
+  r.DELETE("/users/:id", func(c *gin.Context) {
+    user := User{}
+    id := c.Param("id")
+
+    db.Where("ID = ?", id).Delete(&user)
+  })
+
+  r.Run(":8080")
 }
